64bff27400483509aa0dd22e08d121575dea87de
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-18 20:07:34 +0300

    update: rm permisiions for now

--- serverDjango/settings.py
+++ serverDjango/settings.py
@@ -140,7 +140,4 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.SessionAuthentication',
     ],
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.IsAuthenticated',
-    ]
 }
