b94a9f0dcd9edf5bc4b760d1aab68ab22baa0637
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-18 23:47:47 +0300

    rm: rm files as writter creation will be handled by authentication app

--- writter/models.py
+++
@@ -1,7 +0,0 @@
-from fileinput import isstdin
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-
-class WritterModel(AbstractUser):
-    pass
--- writter/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-from .models import WritterModel
-
-
-class WritterSerializer(serializers.ModelSerializer):
-    user_name = serializers.ReadOnlyField(source='user.username')
-    class Meta:
-        model = WritterModel
-        fields = ['isStaff','user_name' ]
