d922c9d15cbef2c67bb7cb289dadeb09f2d4782b
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-18 23:45:06 +0300

    register: custom user and  admin models

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,16 +1,8 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
-from .forms import CustomUserCreationForm, CustomUserChangeForm
-from .models import CustomUser
+from .models import CustomUser,CustomUserAdmin
 
 
-class CustomUserAdmin(UserAdmin):
-    add_form = CustomUserCreationForm
-    form = CustomUserChangeForm
-    model = CustomUser
-    list_display = ["email", "username", ]
 
-# Register your models here.
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
