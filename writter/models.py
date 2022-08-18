b2ace0d62c640cef71c7505bc0b4a8fdb9a0ba95
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-18 20:08:33 +0300

    add: writter model

--- writter/models.py
+++ writter/models.py
@@ -1,9 +1,7 @@
 from fileinput import isstdin
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth.models import AbstractUser
 
 
-class WritterModel(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    isStaff = models.BooleanField(default=False)
-    createdAt = models.DateTimeField(auto_now=True)
+class WritterModel(AbstractUser):
+    pass
