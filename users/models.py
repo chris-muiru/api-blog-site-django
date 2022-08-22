3230006e1772aea2854125255a33e24de5047d62
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-23 00:43:06 +0300

    add: writter model

--- users/models.py
+++ users/models.py
@@ -3,11 +3,16 @@ from django.contrib.auth.models import AbstractUser
 
 
 class CustomUser(AbstractUser):
+
     is_writter = models.BooleanField(default=False)
+
     def __str__(self):
         return self.username
 
 
-# Register your models here.
+class Writter(models.Model):
+    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
 
-# Create your models here.
+    def __str__(self):
+        return self.user.username
+# Register your models here.
