3ca03dd974136ad29850c93f409aa45bce5cdfc3
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-22 11:13:02 +0300

    rm: move functionality to users app

--- authentication/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import CustomUser,CustomUserAdmin
-
-
-
-
-
-admin.site.register(CustomUser, CustomUserAdmin)
--- authentication/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import loginView, registerView
-urlpatterns = [
-    path('login/', loginView),
-    path('signup/', registerView)
-]
