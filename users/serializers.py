9d43caee4c9841e2ff00aa35bcaa8cb8e511223d
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-23 00:44:36 +0300

    update file

--- users/serializers.py
+++ users/serializers.py
@@ -3,7 +3,6 @@ from .models import CustomUser
 
 
 class RegisterSerializer(serializers.ModelSerializer):
-
     class Meta:
         model = CustomUser
         fields = ['username', 'email', 'password']
