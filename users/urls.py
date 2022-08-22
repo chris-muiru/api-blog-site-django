c53cf8ee8475accfa2655432c254ca446fd0fcf8
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-23 00:45:12 +0300

    add: logout routes

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import loginView, registerView
+from .views import loginView, registerView,logoutView
 urlpatterns = [
     path('login/', loginView),
+    path('logout/', logoutView),
     path('signup/', registerView)
 ]
