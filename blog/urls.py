669b052cdde1f50d84e0fb3ebd09922cc34b840c
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-23 18:18:40 +0300

    add: comment and like urlconfs

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import blogView,blogDetaillView
+from .views import blogView, blogDetaillView, likeView, commentView, commentDetailView
 urlpatterns = [
     path('', blogView, name="blog"),
-    path('<int:pk>/', blogDetaillView, name="blogdetail")
+    path('<int:pk>/', blogDetaillView, name="blogdetail"),
+    path('comment/<int:blogid>/', commentView, name="comment"),
+    path('comment/update/<int:commentid>/',
+         commentDetailView, name="commentdetail"),
+    path('like/<int:blogid>/', likeView, name="like"),
 ]
