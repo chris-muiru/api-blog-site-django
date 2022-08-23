a3bb9c6f0b54b0eb6237eb1ac8a437253675c684
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-23 18:18:06 +0300

    add: like,comment and commentdetail views

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,13 @@
-from os import stat
+from urllib import request
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
-from .serializers import BlogSerializer
+from .serializers import BlogSerializer, CommentSerializer, LikeSerializer
 from .permissions import isWritterOrReadOnly
-from .models import BlogModel
+from .models import BlogModel, CommentModel, LikeModel
 
 
 @api_view(['GET', 'POST'])
@@ -41,6 +41,7 @@ def blogDetaillView(request, pk):
     if request.method == 'GET':
         serializer = BlogSerializer(query)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
     elif request.method == 'PUT':
         serializer = BlogSerializer(query, data=request.data)
         if serializer.is_valid():
@@ -51,3 +52,57 @@ def blogDetaillView(request, pk):
     elif request.method == 'DELETE':
         query.delete()
         return Response({'msg': 'deleted'}, status=status.HTTP_200_OK)
+
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated, isWritterOrReadOnly])
+def likeView(request, blogId):
+    if request.method == 'POST':
+        query = BlogModel.objects.filter(id=blogId)
+        # print(query)
+        # print(request.data)
+        if query:
+            serializer = LikeSerializer(data=request.data)
+            print(serializer.is_valid())
+            if serializer.is_valid():
+                serializer.save(user=request.user, blog=query[0])
+                return Response(serializer.data, status=status.HTTP_200_OK)
+    return Response({})
+
+
+@api_view(['POST', 'GET'])
+@permission_classes([IsAuthenticated])
+def commentView(request, blogid):
+    queryset = CommentModel.objects.filter(blog__id=blogid)
+    serializer = CommentSerializer(queryset, many=True)
+    if request.method == 'GET':
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'POST':
+        blogQuery = BlogModel.objects.filter(id=blogid)[0]
+        serializer = CommentSerializer(data=request.data)
+        if serializer.is_valid():
+            checkComment = CommentModel.objects.filter(
+                comment=serializer.validated_data['comment'])
+            if not checkComment:
+                serializer.save(user=request.user, blog=blogQuery)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['DELETE', 'GET', 'PUT'])
+@permission_classes([IsAuthenticated])
+def commentDetailView(request, commentid):
+    query = CommentModel.objects.filter(id=commentid)[0]
+    serializer = CommentSerializer(query)
+    if request.method == 'GET':
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    if request.method == 'PUT':
+        serializer = CommentSerializer(query, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    if request.method == 'DELETE':
+        query.delete()
+        return Response({'comment': 'deleted'}, status=status.HTTP_200_OK)
