79df3b944ed71df0ffa86c7e3ffa2454bf904cd9
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-23 18:17:08 +0300

    add: dunder functions

--- blog/models.py
+++ blog/models.py
@@ -19,10 +19,18 @@ class BlogModel(models.Model):
 class LikeModel(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     blog = models.ForeignKey(BlogModel, on_delete=models.CASCADE)
+    is_liked = models.BooleanField(default=False)
     createdAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.is_liked}"
 
 
 class CommentModel(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     blog = models.ForeignKey(BlogModel, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=100)
     createdAt = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.comment}"
