3f11bb5c92c5aef088d2a59b54af7658f32b831a
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-23 18:19:11 +0300

    register: writter model

--- users/admin.py
+++ users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, Writter
 from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 
@@ -24,3 +24,4 @@ class CustomUserAdmin(UserAdmin):
 
 
 admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Writter)
