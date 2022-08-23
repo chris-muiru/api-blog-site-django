b6963b0b99a3bdea32baa59f86381269fb3c8606
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-23 18:17:25 +0300

    add: like and comment serializers

--- blog/serializers.py
+++ blog/serializers.py
@@ -1,5 +1,6 @@
+from tokenize import Comment
 from rest_framework import serializers
-from .models import BlogModel
+from .models import BlogModel, CommentModel, LikeModel
 
 
 class BlogSerializer(serializers.ModelSerializer):
@@ -9,3 +10,31 @@ class BlogSerializer(serializers.ModelSerializer):
         model = BlogModel
         fields = ['id', 'title', 'content',
                   'blogtype', 'createdAt', 'writterName']
+
+
+class LikeSerializer(serializers.ModelSerializer):
+    blog = serializers.ReadOnlyField(source="blog.title")
+    user = serializers.ReadOnlyField(source="user.username")
+
+    class Meta:
+        model = LikeModel
+        fields = ['blog', 'user', 'is_liked']
+
+    def create(self, validated_data):
+        print(validated_data)
+        query = LikeModel.objects.filter(blog=validated_data['blog'])
+        if query:
+            query[0].is_liked = True
+            query[0].save()
+            return query[0]
+        else:
+            return LikeModel.objects.create(**validated_data)
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    blog = serializers.ReadOnlyField(source="blog.id")
+    user = serializers.ReadOnlyField(source="user.username")
+
+    class Meta:
+        model = CommentModel
+        fields = ['id', 'blog', 'user', 'comment', 'createdAt']
