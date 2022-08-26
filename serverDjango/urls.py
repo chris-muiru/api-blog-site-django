bf5a0cd71ac41a83ae0d575e07258ee0d121a238
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-27 02:54:25 +0300

    add: simplejwt api ufconfs and apiviews

--- serverDjango/urls.py
+++ serverDjango/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include("users.urls")),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('blog/', include("blog.urls")),
     # login and logout for browsable api
     path('apiAuth', include("rest_framework.urls"))
