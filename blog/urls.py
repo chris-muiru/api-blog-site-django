95d8c84c187830ec9fd1b53d8952098a5e953011
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-27 16:20:05 +0300

    fix: spelling mistake

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import blogView, blogDetaillView, likeView, commentView, commentDetailView
+from .views import blogView, blogDetailView, likeView, commentView, commentDetailView
 urlpatterns = [
     path('', blogView, name="blog"),
-    path('<int:pk>/', blogDetaillView, name="blogdetail"),
+    path('<int:pk>/', blogDetailView, name="blogdetail"),
     path('comment/<int:blogid>/', commentView, name="comment"),
     path('comment/update/<int:commentid>/',
          commentDetailView, name="commentdetail"),
