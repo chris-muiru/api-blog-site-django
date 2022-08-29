734879ce93316eee23b76347ed9d010ff7ec67d6
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-29 15:08:11 +0300

    add: isPermitted urlConf to call cancreateblog view

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import blogView, blogDetailView, likeView, commentView, commentDetailView
+from .views import blogView, blogDetailView, likeView, commentView, commentDetailView, canCreateBlog
 urlpatterns = [
     path('', blogView, name="blog"),
     path('<int:pk>/', blogDetailView, name="blogdetail"),
+    path('isPermitted/', canCreateBlog),
     path('comment/<int:blogid>/', commentView, name="comment"),
     path('comment/update/<int:commentid>/',
          commentDetailView, name="commentdetail"),
