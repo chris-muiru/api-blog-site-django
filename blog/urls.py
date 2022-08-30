aa84e289a0821c4035ee1642986dcfc187032843
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-30 23:44:06 +0300

    add: isPermitted urf conf and use camelcase in url parameter names

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import blogView, blogDetailView, likeView, commentView, commentDetailView, canCreateBlog
+from .views import blogView, blogDetailView, likeView, commentView, commentDetailView, userIsWritter, accessToCrudFunctionalityOnBlogId
 urlpatterns = [
     path('', blogView, name="blog"),
     path('<int:pk>/', blogDetailView, name="blogdetail"),
-    path('isPermitted/', canCreateBlog),
-    path('comment/<int:blogid>/', commentView, name="comment"),
-    path('comment/update/<int:commentid>/',
-         commentDetailView, name="commentdetail"),
-    path('like/<int:blogid>/', likeView, name="like"),
+    path('isPermitted/', userIsWritter),
+    path('isPermitted/<int:blogId>/', accessToCrudFunctionalityOnBlogId),
+    path('comment/<int:blogId>/', commentView, name="comment"),
+    path('comment/update/<int:commentId>/',
+         commentDetailView, name="commentDetail"),
+    path('like/<int:blogId>/', likeView, name="like"),
 ]
