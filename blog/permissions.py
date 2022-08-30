262317b77368a0639b3cb99bca2bccada2a27fb4
kris-slinger <krisdjangodeveloper@gmail.com>
2022-08-30 23:41:31 +0300

    rm: has_blog_permission method since its only applicable in generic views

--- blog/permissions.py
+++ blog/permissions.py
@@ -1,15 +1,8 @@
 from rest_framework import permissions
 
+
 class isWritterOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         if request.method in permissions.SAFE_METHODS:
             return True
-
-        if request.user.is_writter == True:
-            return True
-        return False
-
-    def has_object_permission(self, request, view, obj):
-        if request.user.is_writter:
-            return True
-        return False
+        return request.user.is_writter == True
